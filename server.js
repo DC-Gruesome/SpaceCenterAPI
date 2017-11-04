909d23addc65fe30dfb73901a58fd06d8d9c3959
Naytun <naytunthein70@gmail.com>
2017-11-04 17:20:01 -0400

    commit

--- server.js
+++ server.js
@@ -42,46 +42,66 @@ db.on('connect', function() {
 
 //======================= API Functions ===============================
 
+app.get('/news/all', rssfeed_all);
 app.get('/news/spacenews', rssfeed_spacenews);
 app.get('/news/space', rssfeed_space);
+app.get('/news/universetoday', rssfeed_universetoday);
 
-function rssfeed_spacenews(req, res){
+function rssfeed_all(req, res){
     feed("http://spacenews.com/feed/", function(err, articles) {
-        console.log(err);
         if (err) return err;
-        // Each article has the following properties:
-        // 
-        //   * "title"     - The article title (String).
-        //   * "author"    - The author's name (String).
-        //   * "link"      - The original article link (String).
-        //   * "content"   - The HTML content of the article (String).
-        //   * "published" - The date that the article was published (Date).
-        //   * "feed"      - {name, source, link}
-        // 
-        return res.status(200).send(articles);
+        var rssfeed_spacenews = rssfeed_spacenews();
+
+        feed("https://www.space.com/home/feed/site.xml", function(err, articles) {
+            if (err) return err;
+            var rssfeed_space = articles;
+
+            // Combine all rss feeds
+            var rssfeed_all = [];
+            rssfeed_all.push(rssfeed_spacenews);
+            rssfeed_all.push(rssfeed_space);
+
+            // console.log(rssfeed_all);
+            // Each article has the following properties:
+            // 
+            //   * "title"     - The article title (String).
+            //   * "author"    - The author's name (String).
+            //   * "link"      - The original article link (String).
+            //   * "content"   - The HTML content of the article (String).
+            //   * "published" - The date that the article was published (Date).
+            //   * "feed"      - {name, source, link}
+            // 
+            return res.status(200).send(rssfeed_all);
+        });    });
+}
+
+function rssfeed_spacenews(){
+    feed("http://spacenews.com/feed/", function(err, articles) {
+        if (err) return err;
+        return (articles);
       });
           
 }
 
-function rssfeed_space(req, res){
+function rssfeed_space(){
     feed("https://www.space.com/home/feed/site.xml", function(err, articles) {
         console.log(err);
         if (err) return err;
-        // Each article has the following properties:
-        // 
-        //   * "title"     - The article title (String).
-        //   * "author"    - The author's name (String).
-        //   * "link"      - The original article link (String).
-        //   * "content"   - The HTML content of the article (String).
-        //   * "published" - The date that the article was published (Date).
-        //   * "feed"      - {name, source, link}
-        // 
         // console.log(articles);
+        return (articles);
+    });
+            
+}
+    
+function rssfeed_universetoday(req, res){
+    feed("https://www.universetoday.com/universetoday.xml", function(err, articles) {
+        console.log(err);
+        if (err) return err;
+        console.log(articles);
         return res.status(200).send(articles);
         });
             
 }
-    
 
 //------------------------- Server start listening ----------------------
 app.listen(port);
